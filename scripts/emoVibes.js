bbeb6daff1069392c17c610b97268903f4aba7aa
jfonseca8 <fonseca@uchicago.edu>
2019-10-21 15:06:03 -0500

    fixed tap-gesture

---
+++ scripts/emoVibes.js
@@ -0,0 +1,26 @@
+// These are the vibration patterns that should have an emotional correspondence
+// to the emojis that they represent. These are subjective.
+
+function emotionalAngryEmoji() {
+  //navigator.vibrate([500, 250, 500, 250, 500, 250, 500, 250, 500, 250, 500]);
+}
+
+function emotionalSadEmoji() {
+  //navigator.vibrate([500, 250, 500, 250, 500, 250, 500, 250, 500, 250, 500]);
+}
+
+function emotionalHeartEmoji() {
+  //navigator.vibrate([500, 250, 500, 250, 500, 250, 500, 250, 500, 250, 500]);
+}
+
+function emotionalLikeEmoji() {
+  //navigator.vibrate([500, 250, 500, 250, 500, 250, 500, 250, 500, 250, 500]);
+}
+
+function emotionalLaughEmoji() {
+  //navigator.vibrate([500, 250, 500, 250, 500, 250, 500, 250, 500, 250, 500]);
+}
+
+function emotionalEmoji() {
+  //navigator.vibrate([500, 250, 500, 250, 500, 250, 500, 250, 500, 250, 500]);
+}
--- scripts/tap-gesture.js
+++ scripts/tap-gesture.js
@@ -1,21 +1,25 @@
-/* temporary script from hammer.js example */
+// We have to include this to make sure that the content has loaded!
+document.addEventListener('DOMContentLoaded', function() {
 
-// Get a reference to an element
-var square = document.querySelector('.square');
+  // Get a reference to an element
+  var square = document.querySelector('.square');
+  console.log(square);
 
-// Create a manager to manager the element
-var manager = new Hammer.Manager(square);
+  // Create a manager to manage the element
+  var manager = new Hammer.Manager(square);
 
-// Create a recognizer
-var Press = new Hammer.Press({
-  time: 500
-});
+  // Create a recognizer
+  var Tap = new Hammer.Tap({
+    taps: 1
+  });
 
-// Add the recognizer to the manager
-manager.add(Press);
+  // Add the recognizer to the manager
+  manager.add(Tap);
 
-// Subscribe to desired event
-manager.on('press', function(e) {
-  e.target.classList.toggle('expand');
-});
+  // Subscribe to the desired event
+  manager.on('tap', function(e) {
+    e.target.classList.toggle('expand');
+    console.log("you tapped");
+  });
 
+}, false);
