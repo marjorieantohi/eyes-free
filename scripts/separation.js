1006c1dd7681c711bcee28c81fc3129d1bb319c4
mouse111 <mandalamouse@gmail.com>
2019-10-22 15:31:49 -0500

    did some work on adding the numerical gestures

---
+++ scripts/separation.js
@@ -0,0 +1,23 @@
+  function hideVibrationClass() {
+    document.getElementsByClassName('beginVibration')[0].style.visibility = 'hidden';
+  }
+
+  function showVibrationClass() {
+    document.getElementsByClassName('beginVibration')[0].style.visibility = '';
+  }
+
+  function hideGestureClass() {
+    document.getElementsByClassName('beginGesture')[0].style.visibility = 'hidden';
+  }
+
+  function showGestureClass() {
+    document.getElementsByClassName('beginGesture')[0].style.visibility = '';
+  }
+
+  function hideGestureSquaresClass() {
+    document.getElementsByClassName('gestureSpace')[0].style.visibility = 'hidden';
+  }
+
+  function showGestureSquaresClass() {
+    document.getElementsByClassName('gestureSpace')[0].style.visibility = '';
+  }
